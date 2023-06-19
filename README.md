9fbae28e7b1d50677f1cca6892223cbd6fe7658c
macasensio <macasensio@gmail.com>
2023-06-19 18:57:44 +0200

    uploading project

---
+++ README.md
@@ -0,0 +1,14 @@
+## Crypto - API
+- DOM
+- Fetch()
+- Async Await
+
+
+### Technologies
+- VanillaJS
+- HTML5
+- CSS3
+
+##### API - Cryptocompare
+
+[https://min-api.cryptocompare.com/documentation](https://min-api.cryptocompare.com/documentation)
---
+++ js/index.js
@@ -0,0 +1,114 @@
+//variables
+const monedaSelect = document.querySelector('#moneda')
+const criptoSelect = document.querySelector('#criptomoneda')
+
+const formulario = document.querySelector('#formulario')
+const resultado = document.querySelector('.resultado')
+
+//objeto consulta
+const objConsulta = {
+    moneda: '',
+    criptomoneda: ''
+}
+
+//addEventListener
+document.addEventListener('DOMContentLoaded', () => {
+    consultarApi()
+    monedaSelect.addEventListener('change', leerValor)
+    criptomoneda.addEventListener('change', leerValor) 
+    formulario.addEventListener('submit', submitFormulario)
+})
+
+
+//leer valor del select
+function leerValor(e){
+    objConsulta[e.target.name] = e.target.value
+}
+
+function submitFormulario(e){
+    e.preventDefault()
+
+    //validar form
+    const {moneda, criptomoneda} = objConsulta
+    if(moneda === '' || criptomoneda === ''){
+        mostrarAlerta('Ambos campos son obligatorios')
+        return
+    }
+    //consultar API resultados
+    consultarResultado()
+}
+
+function mostrarAlerta(msg){
+    limpiarHTML()
+    const alerta = document.querySelector('.alerta')
+
+    if (!alerta) {
+        const alerta = document.createElement('p')
+        alerta.classList.add('alerta')
+        alerta.textContent = msg
+        resultado.appendChild(alerta)
+
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000)
+    }
+}
+
+function consultarResultado(){
+    const {moneda, criptomoneda} = objConsulta
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+    console.log(url)
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => mostrarHTML(cotizacion.DISPLAY[criptomoneda][moneda]))
+        .catch(error => console.error(error))
+}
+
+
+function mostrarHTML(cotizacion){
+    const { LOW24HOUR, HIGH24HOUR, PRICE} = cotizacion
+    console.log(cotizacion)
+    limpiarHTML()
+    const respuesta = document.createElement('div')
+    respuesta.innerHTML = `
+        <p class="precio">Precio actual: <span>${PRICE}</span></p>
+        <p>Precio más bajo en las últimas 24hs: <span>${LOW24HOUR}</span></p>
+        <p>Precio más alto en las últimas 24hs: <span>${HIGH24HOUR}</span></p>
+    `
+
+    resultado.appendChild(respuesta)
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild)
+    }
+}
+
+
+//consultar criptos a la Api
+function consultarApi(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+    
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            const criptomonedas = resultado.Data
+            selectCriptomonedas(criptomonedas)
+        })
+        .catch(error => console.error(error))
+}
+
+//selectCriptomonedas
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const {FullName, Name} = cripto.CoinInfo
+
+        const option = document.createElement('option')
+        option.value = Name
+        option.textContent = FullName
+
+        criptoSelect.appendChild(option)
+    })
+}
